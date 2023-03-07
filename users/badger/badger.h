e13ea0470234c5892173c98ff217fa5256dec5d5
Dan White <github@bluetufa.com>
2023-03-06 20:15:03 -0700

    Ketch layout

--- users/badger/badger.h
+++ users/badger/badger.h
@@ -56,6 +56,9 @@ enum CustomKeys {
 #define MAC_DUP    LGUI(KC_D)
 #define MAC_ALL    LGUI(KC_A)
 #define MAC_SAV    LGUI(KC_S)
+#define MAC_B      LGUI(KC_B)
+#define MAC_FND    LGUI(KC_F)
+#define MAC_WND    LGUI(KC_GRV)
 #define KC_BACK    LGUI(KC_LBRC)
 #define KC_NEXT    LGUI(KC_RBRC)
 
@@ -67,6 +70,7 @@ enum CustomKeys {
 #define LT_FUNC    LT(_FUNCTION, KC_LEFT)
 #define CONFIG     MO(_CONFIG)
 #define J_MACRO    MO(_MACRO)
+#define J_FUNCT    MO(_FUNCTION)
 #define CFG_LNX    MO(_CONFIG)
 
 #define WD_BACK    LALT(KC_LEFT)
@@ -129,6 +133,7 @@ enum CustomKeys {
 #define VS_LINE    LGUI(KC_L)
 #define VS_MVUP    LSFT(LALT(KC_UP))
 #define VS_MVDN    LSFT(LALT(KC_DOWN))
+#define VS_JSFT    RSFT_T(LGUI(LSFT((KC_P))))
 
 /* MacOS virtual desktop shortcuts */
 #define M_VD1      LCTL(KC_1)
